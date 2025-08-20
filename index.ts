5a76f97d2f8e206195160562427a412b73fab33a
isochikuwa <isochikuwa0522@gmail.com>
2025-08-21 07:32:48 +0900

    使用しなくなったメソッドを削除

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-
 const TILE_SIZE = 30;
 const FPS = 30;
 const SLEEP = 1000 / FPS;
@@ -19,52 +18,28 @@ enum RawInput {
 }
 
 interface Input {
-  isRight(): boolean;
-  isLeft(): boolean;
-  isUp(): boolean;
-  isDown(): boolean;
   handle(): void;
 }
 
 class Right implements Input {
-  isRight() { return true; }
-  isLeft() { return false; }
-  isUp() { return false; }
-  isDown() { return false; }
-
   handle() {
     moveHorizontal(1);
   }
 }
 
 class Left implements Input {
-  isRight() { return false; }
-  isLeft() { return true; }
-  isUp() { return false; }
-  isDown() { return false; }
-
   handle() {
     moveHorizontal(-1);
   }
 }
 
 class Up implements Input {
-  isRight() { return false; }
-  isLeft() { return false; }
-  isUp() { return true; }
-  isDown() { return false; }
-
   handle() {
     moveVertical(-1);
   }
 }
 
 class Down implements Input {
-  isRight() { return false; }
-  isLeft() { return false; }
-  isUp() { return false; }
-  isDown() { return true; }
-
   handle() {
     moveVertical(1);
   }
